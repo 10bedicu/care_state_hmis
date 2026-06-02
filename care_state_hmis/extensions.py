6cd72332814f41a05dd00c78499ec46d6aa87ead
Jacob Jeevan <mail@jacobjeevan.me>
2026-06-02 21:50:39 +0530

    ft: attender extension

--- care_state_hmis/extensions.py
+++ care_state_hmis/extensions.py
@@ -34,13 +34,17 @@ class PatientDemographicsExtension(PlugExtension):
         "properties": {
             "related_person": {"type": "string", "title": "Related Person"},
             "caste": {
-                "x-ui": {"render_blacklist": ["treatment_summary", "appointment_print"]},
+                "x-ui": {
+                    "render_blacklist": ["treatment_summary", "appointment_print"]
+                },
                 "type": "string",
                 "title": "Caste",
                 "enum": ["OBC", "General", "SC", "ST", "Other"],
             },
             "religion": {
-                "x-ui": {"render_blacklist": ["treatment_summary", "appointment_print"]},
+                "x-ui": {
+                    "render_blacklist": ["treatment_summary", "appointment_print"]
+                },
                 "type": "string",
                 "title": "Religion",
                 "enum": ["Hindu", "Muslim", "Christian", "Sikh", "Jain", "Other"],
@@ -50,4 +54,60 @@ class PatientDemographicsExtension(PlugExtension):
     }
 
 
+class EncounterAttenderExtension(PlugExtension):
+    extension_name = "encounter_attender"
+    extension_version = "1.0.0"
+    resource_type = ExtensionResource.encounter
+    write_schema = {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "title": "Encounter Attender",
+        "type": "object",
+        "properties": {
+            "attender": {
+                "type": "object",
+                "title": "Attender Details",
+                "x-ui": {"control": "grid"},
+                "properties": {
+                    "attender_relation": {
+                        "type": "string",
+                        "title": "Attender Relation",
+                        "enum": ["S/O","D/O","F/O","M/O","W/O","H/O","Guardian/O"],
+                    },
+                    "attender_name": {"type": "string", "title": "Attender Name"},
+                    "attender_phone": {
+                        "type": "string",
+                        "title": "Attender Phone Number",
+                    },
+                    "attender_address": {"type": "string", "title": "Attender Address"},
+                },
+            }
+        },
+        "additionalProperties": "false",
+    }
+    retrieve_schema = {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "title": "Encounter Attender",
+        "type": "object",
+        "properties": {
+            "attender": {
+                "type": "object",
+                "title": "Attender Details",
+                "x-ui": {"control": "grid"},
+                "properties": {
+                    "attender_relation": {
+                        "type": "string",
+                        "title": "Attender Relation",
+                        "enum": ["S/O","D/O","F/O","M/O","W/O","H/O","Guardian/O"],
+                    },
+                    "attender_name": {"type": "string", "title": "Attender Name"},
+                    "attender_phone": {"type": "string", "title": "Attender Phone Number"},
+                    "attender_address": {"type": "string", "title": "Attender Address"},
+                },
+            }
+        },
+        "additionalProperties": "false",
+    }
+
+
 ExtensionRegistry.register(PatientDemographicsExtension())
+ExtensionRegistry.register(EncounterAttenderExtension())
